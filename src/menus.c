11042f70a238c08dacd414009ca64623116e6907
Daveee10 <daver16@msn.com>
2016-09-11 13:06:35 +0200

    Reorder Screen Savers Now the first is the logo then the Analog and the Digital Clock after clocks the animated savers...

--- src/menus.c
+++ src/menus.c
@@ -958,10 +958,10 @@ const menu_t ScreenSaveMenu =
 	6,
 	{
 		{ String_None, 0, 1, 0 },
+		{ String_LOGO, 0, 1, 0 },
 		{ String_Analog_Clk, 0, 1, 0 },
 		{ String_Digit_Clk, 0, 1, 0 },
 		{ String_Cube, 0, 1, 0 },
-		{ String_LOGO, 0, 1, 0 },
 		{ String_Qix, 0, 1, 0 }
 	}
 };
